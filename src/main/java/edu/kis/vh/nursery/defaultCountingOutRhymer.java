de4edd8774efb13cfe495475e1461f52762b730c
Filip Barszczyk <245764@edu.p.lodz.pl>
2024-05-27 15:40:35 +0200

    Zadanie rownolegle

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,19 +2,24 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    public static final int minusOne = -1;
-    public static final int eleven = 11;
-    public static final int numLength = 12;
+    private static final int minusOne = -1;
+    private static final int eleven = 11;
+    private static final int numLength = 12;
     private int[] NUMBERS = new int[numLength];
+
+    public int getTotal() {
+        return total;
+    }
+
 
     public int total = minusOne;
 
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-        public boolean callCheck() {
+        protected boolean callCheck() {
             return total == minusOne;
         }
         
@@ -28,7 +33,7 @@ public class defaultCountingOutRhymer {
                     return NUMBERS[total];
                 }
             
-                    public int countOut() {
+                    protected int countOut() {
                         if (callCheck())
                             return minusOne;
                         return NUMBERS[total--];
