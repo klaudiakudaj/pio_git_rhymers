0fb02c7794f17f1224dd3a6b000dbe415a9f5164
klaudiakudaj <245854@edu.p.lodz.pl>
2024-06-03 15:06:46 +0200

    17. Dodanie dokumentacji do klasy HanoiRhymer i jej metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,18 +1,34 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Reprezentuje specjalizowaną wersję klasy defaultCountingOutRhymer,
+ * która zlicza, ile razy liczba została odrzucona zgodnie z określonymi warunkami.
+ */
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private int totalRejected = 0;
 
+    /**
+     * Zwraca łączną liczbę odrzuconych liczb.
+     *
+     * @return łączna liczba odrzuconych liczb.
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Dodaje podaną liczbę. Jeśli liczba jest większa niż liczba obecnie na szczycie stosu
+     * i stos nie jest pusty, liczba zostaje odrzucona, a licznik odrzuceń jest zwiększany.
+     * W przeciwnym razie liczba jest dodawana do stosu.
+     *
+     * @param in liczba do dodania.
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
             super.countIn(in);
-        }
+    }
 }
