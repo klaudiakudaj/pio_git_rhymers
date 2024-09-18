0a455bfff261abafbcc6e7cabddbbe2a48b33d8e
klaudiakudaj <117376601+klaudiakudaj@users.noreply.github.com>
2024-09-18 09:22:26 +0200

    Update HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,9 +2,9 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+   private int totalRejected = 0;
 
-    protected int reportRejected() {
+    public int reportRejected() {
         return totalRejected;
     }
 
