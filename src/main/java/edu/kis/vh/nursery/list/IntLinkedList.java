9e8a638d27e78071c70f12e7652a64e6608afe0e
klaudiakudaj <117376601+klaudiakudaj@users.noreply.github.com>
2024-09-18 09:28:03 +0200

    Update IntLinkedList.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,40 +1,42 @@
 package edu.kis.vh.nursery.list;
+class Node{
+    public int value;
+    public Node prev, next;
 
+    public Node(int i) {
+        value = i;
+    }
+
+}
 public class IntLinkedList {
 
-    private Node last;
-    private int i;
-
+    Node last;
+    int i;
     public void push(int i) {
         if (last == null)
             last = new Node(i);
         else {
-            last.setNext(new Node(i));
-            last.getNext().setPrev(last);
-            last = last.getNext();
+            last.next = new Node(i);
+            last.next.prev = last;
+            last = last.next;
         }
     }
-
     public boolean isEmpty() {
         return last == null;
     }
-
     public boolean isFull() {
         return false;
     }
-
     public int top() {
         if (isEmpty())
             return -1;
-        return last.getValue();
+        return last.value;
     }
-
     public int pop() {
         if (isEmpty())
             return -1;
-        int ret = last.getValue();
-        last = last.getPrev();
+        int ret = last.value;
+        last = last.prev;
         return ret;
     }
-
 }
